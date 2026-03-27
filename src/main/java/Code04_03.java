742e3b402c285516f571ce460993bbf80d07edc1
Hello2 <msjeon9983@gmail.com>
2026-03-27 14:45:28 +0900

    Code04_03

---
+++ src/main/java/Code04_03.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Code04_03 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        int n1 =1;
+        int n2 =2;
+
+        double reseult = (double) n1 / n2;
+
+        System.out.println(n1 + "/" + n2 + "=" + reseult);
+    }
+}
