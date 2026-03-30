df12892946dc92921740280bff29d83c1f4c1c1c
Hello2 <msjeon9983@gmail.com>
2026-03-30 14:14:18 +0900

    Exam02

---
+++ src/main/java/Exam02.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exam02 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        String a = s.next();
+        String b = s.next();
+
+        String result = a + b;
+
+        result = result.toLowerCase();
+        result = result.replace(" ","");
+
+        System.out.println(result);
+
+        s.close();
+    }
+}
