e771d8c393b3911adc367d400ad4f4f3da84046e
Hello2 <msjeon9983@gmail.com>
2026-03-30 14:45:28 +0900

    Exam02

--- src/main/java/Exam02.java
+++ src/main/java/Exam02.java
@@ -4,9 +4,9 @@ public class Exam02 {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
 
-        String ss = s.nextLine();
+        String sc = s.nextLine();
 
-       String var1 = ss.toLowerCase();
+       String var1 = sc.toLowerCase();
         var1 = var1.replaceAll(" ","");
 
         System.out.println(var1);
