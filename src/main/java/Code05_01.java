daeea08e4f315fdde3b8837605c63b4ba9d8be30
Hello2 <msjeon9983@gmail.com>
2026-03-31 10:39:31 +0900

    Code05_05

--- src/main/java/Code05_01.java
+++ src/main/java/Code05_01.java
@@ -2,7 +2,8 @@ public class Code05_01 {
     public static void main(String[] args){
         int num = 99;
 
-        if(num < 100)
+        if(num < 100) {
             System.out.println("100보다 작습니다.");
+        }
     }
 }
--- src/main/java/Code05_05.java
+++ src/main/java/Code05_05.java
@@ -8,10 +8,11 @@ public class Code05_05 {
         System.out.print("숫자 입력 ==> ");
         num = s.nextInt();
 
-        if(num%2 == 0)
+        if(num%2 == 0) {
             System.out.println("짝수입니다.");
-        else
+        } else {
             System.out.println("홀수입니다.");
+        }
 
         s.close();
     }
