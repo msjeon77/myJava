68a4f3aaa84842a0e6cdcdb98029bcb653829c96
msjeon77 <msjeon9983@gmail.com>
2026-04-17 14:24:04 +0900

    code07_06

--- src/main/java/Code07_06.java
+++ src/main/java/Code07_06.java
@@ -11,6 +11,17 @@ class Rabbit{
 
 public class Code07_06 {
     public static void main(String[] args) {
+        Rabbit rabbit1 = new Rabbit();
+        Rabbit rabbit2 = new Rabbit();
+        Rabbit rabbit3 = new Rabbit();
+
+        rabbit1.shape = "원";
+        rabbit2.shape = "삼각형";
+        rabbit3.shape = "토끼";
+
+        rabbit1.setPosition(100, 100);
+        rabbit2.setPosition(-100, 100);
+        rabbit3.setPosition(0, -100);
 
     }
 }
