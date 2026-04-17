8a89bf8e461caae7a07b8126382ffa4528ecb0ae
msjeon77 <msjeon9983@gmail.com>
2026-04-17 15:14:28 +0900

    code07_06

--- src/main/java/Code07_06.java
+++ src/main/java/Code07_06.java
@@ -3,6 +3,10 @@ class Rabbit{
     int xPos;
     int yPos;
 
+    Rabbit(String value) {
+        shape = value;
+    }
+
     void setPosition(int x, int y) {
         xPos = x;
         yPos = y;
@@ -11,9 +15,10 @@ class Rabbit{
 
 public class Code07_06 {
     public static void main(String[] args) {
-        Rabbit rabbit1 = new Rabbit();
-        Rabbit rabbit2 = new Rabbit();
-        Rabbit rabbit3 = new Rabbit();
+
+        Rabbit rabbit1 = new Rabbit("원");
+        Rabbit rabbit2 = new Rabbit("삼각형");
+        Rabbit rabbit3 = new Rabbit("토끼");
 
         rabbit1.shape = "원";
         rabbit2.shape = "삼각형";
@@ -23,5 +28,8 @@ public class Code07_06 {
         rabbit2.setPosition(-100, 100);
         rabbit3.setPosition(0, -100);
 
+        System.out.printf("토끼1 객채의 모양은 [%s]입니다.\n", rabbit1.shape);
+        System.out.printf("토끼2 객채의 모양은 [%s]입니다.\n", rabbit2.shape);
+        System.out.printf("토끼3 객채의 모양은 [%s]입니다.\n", rabbit3.shape);
     }
 }
